0bf6ee361a7b3abe729d4e1eeaa45816c667b10c
Torren Sampson <torren.sampson@gmail.com>
2019-05-22 11:20:08 -0700

    Added time_step_to_enqueue and checkpoint_target variables, describe find checkpoint

--- attendee.py
+++ attendee.py
@@ -10,8 +10,24 @@ class Attendee(object):
         self.time_entered = time_entered
         self.has_bag = has_bag
         self.is_cooperative = is_cooperative
+        self.time_step_to_enqueue = 0
+        self.checkpoint_target = None
     
-    def findQueue(self, checkpoints):
+    def findCheckpoint(self, checkpoints):
+        """Finds a checkpoint based on proximity and checkpoint queue size 
+        
+        The first factor is the proximity of a checkpoint, if a checkpoint is close, line length will be checked
+        if length of the line is above a certain tolerance, a further line will be checked. Attenedee will attempt to find 
+        the closest checkpoint with the shortest line. If no line is short enough, the agent will use the closest line. 
+           
+        Variables
+        Checkpoints: List of open checkpoints in current security configuration
+
+        Sets the time_step_to_enque and the checkpoint_target based on the checkpoint the agent decides to go to   
+            """
+        pass 
     
-    def calcTotalWait(self, time_waiting)
+    def calcTotalWait(self, time_waiting):
+        pass
+
 
