af478961aaf7da54362c5d7fdaf5d382ac31ce9a
Samash92 <tsampson2092@gmail.com>
2019-05-29 00:32:46 -0700

    add beginnings of a spawnpoint class, more to come

---
+++ security_simulation/spawnpoint.py
@@ -0,0 +1,45 @@
+import numpy as N
+import numpy.random as rand
+
+from security_simulation.attendee import Attendee
+
+
+
+class SpawnPoint(object):
+
+    def __init__(self, spawn_chance, spawn_more_than_one_chance, attendee_init_params, max_spawn=3, location=(0,0)):
+        """
+        Defines an attendee entrance (spawn) point into the simulation
+        :param spawn_chance: defines the chance that an attendee will spawn at any give time step. values in range [0,1)
+        :param spawn_more_than_one_chance: chance that more than one attendee will spawn at a single spawn point, in 
+               any given spawn point. values in range [0,1)
+        :param attendee_init_params: a list of parameters that will define attendees, the order of parameters being the exact order
+               of parameters in the attendee class __init__ method
+        :param max_spawn: The maximum number of attendees that could be spawned at any given time step defaults->3
+        :param location: They (y,x) location of the spawn point defaults->(0,0)
+        """
+        self.spawn_chance = spawn_chance
+        self.spawn_more_than_one_chance = spawn_more_than_one_chance
+        self.attendee_init_params = attendee_init_params
+        self.max_spawn = max_spawn
+        self.location = location
+
+    
+    def spawn_attendee(self, current_time_step):
+        spawned_attendies = []
+        if rand.random() > self.spawn_chance:
+            if (rand.random() > self.spawn_more_than_one_chance):
+                num_to_spawn = rand.randint(2, self.max_spawn)
+            else:
+                num_to_spawn = 1
+            for i in range(num_to_spawn):
+                enter_ye = Attendee(self.attendee_init_params[0],\
+                                    self.attendee_init_params[1],\
+                                    self.attendee_init_params[2],\
+                                    self.attendee_init_params[3],\
+                                    current_location=self.location,\
+                                    time_entered=current_time_step,\
+                                    has_bag=rand.randint(0,1))
+                spawned_attendies.append(enter_ye)
+        return spawned_attendies, num_to_spawn
+                
